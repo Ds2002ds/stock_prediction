62d62a7c29c9e00735f531c5d5067d0a23aa1abb
DEEPANSHU SHARMA <132010657+Ds2002ds@users.noreply.github.com>
2025-12-08 15:24:27 +0530

    Update Single Candlestick.py

--- pages/Single Candlestick.py
+++ pages/Single Candlestick.py
@@ -4,3 +4,4 @@ st.set_page_config(page_title="Single Candlestick")
 st.title("Single Candlestick Working ✅")
 
 st.write("If you see this, navigation works.")
+st.page_link("pages/Single Candlestick.py", label="Single Candlestick")
