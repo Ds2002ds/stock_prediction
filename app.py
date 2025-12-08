3f78abbff3fd8579130b4afca241d9a43e9b1e90
DEEPANSHU SHARMA <132010657+Ds2002ds@users.noreply.github.com>
2025-12-08 15:17:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ with open("ind.html", "r", encoding="utf-8") as f:
     html = f.read()
 
 st.components.v1.html(html, height=800, scrolling=True)
-st.page_link("pages/single_Candlestick.py", label="single Candlestic")
+st.page_link("pages/single_candlestick.py", label="single Candlestic")
