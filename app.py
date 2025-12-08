319a312fc9b8d4efd9ab1ba8e476f0a834b04a70
DEEPANSHU SHARMA <132010657+Ds2002ds@users.noreply.github.com>
2025-12-08 15:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+st.title("🏠 Home")
+
 with open("ind.html", "r", encoding="utf-8") as f:
     html = f.read()
 
 st.components.v1.html(html, height=800, scrolling=True)
-
