415de44c1ba28f24173b76a0894eeb6c3e457add
DEEPANSHU SHARMA <132010657+Ds2002ds@users.noreply.github.com>
2025-12-08 14:53:29 +0530

    Create Double_Candlestick.py

---
+++ pages/Double_Candlestick.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Page title
+st.title("📊 Double Candlestick Pattern")
+
+st.write(
+    """
+    Double candlestick patterns use two consecutive candles 
+    to predict possible market reversals or continuation.
+    """
+)
